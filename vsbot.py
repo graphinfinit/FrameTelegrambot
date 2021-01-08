4fee70aec746543c222061c81fec045ff8558633
graphinfinit <dissociativetransgression@gmail.com>
2021-01-08 21:23:59 +0300

    'ghj'

--- vsbot.py
+++ vsbot.py
@@ -75,6 +75,7 @@ def create_inlinekeyboarb(message):
 
     for shift in SHIFT_INTERVALS:
         text = SHIFT_INTERVALS[shift]
+        print('problems ///')
         try:
             us = len(db.count_rows(shift=shift))
         except Excepion as asf:
